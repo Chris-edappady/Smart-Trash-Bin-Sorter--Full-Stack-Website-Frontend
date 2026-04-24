9a9e7d404f02fa801cb4e4e6f07eb10b1fff69ac
Chris-edappady <chris.edappady123@gmail.com>
2026-04-24 19:20:37 +0400

    Update 5: Added colors for each category and improved progress ring

--- vue.js
+++ vue.js
@@ -3,10 +3,10 @@ new Vue({
 
   data: {
     categories: [
-      { name: 'Paper', level: 0, count: 0 },
-      { name: 'Plastic', level: 0, count: 0 },
-      { name: 'Metal', level: 0, count: 0 },
-      { name: 'General', level: 0, count: 0 }
+      { name: 'Paper', level: 0, count: 0, color: '#2b8aee' },
+      { name: 'Plastic', level: 0, count: 0, color: '#f59e0b' },
+      { name: 'Metal', level: 0, count: 0,  color: '#6b7280' },
+      { name: 'General', level: 0, count: 0, color:'#10b981' }
     ]
   }
 });
