d148d511bb9fdfc1f1d0034596f592f9c0b5699c
Chris-edappady <chris.edappady123@gmail.com>
2026-04-24 18:22:04 +0400

    Update 3: Integrated Vue.js for category cards

---
+++ vue.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+
+  data: {
+    categories: [
+      { name: 'Paper', level: 0, count: 0 },
+      { name: 'Plastic', level: 0, count: 0 },
+      { name: 'Metal', level: 0, count: 0 },
+      { name: 'General', level: 0, count: 0 }
+    ]
+  }
+});
